6f81f9b9d314472d4737c3ce19917ae33b250aa5
Vini <viniciusnunesmollas@hotmail.com>
2024-10-17 11:57:55 -0300

    Finish the feature for random colors

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 "use strict"
 const containers = document.querySelectorAll('.container');
 const input = document.querySelector('.slide');
+const rainbow = document.querySelector('.rainbow');
+const pencil = document.querySelector('.pencil')
 let mouseState = false;
+let randomState = false;
+
+window.addEventListener("mouseup", () => {
+    mouseState = false;
+})
 
 function createGrid (n) {
     containers.forEach(container => {
@@ -12,32 +19,48 @@ function createGrid (n) {
             pixel.addEventListener("mousedown", (e) => {
                 if (e.button === 0) {
                     mouseState = true;
-                    pixel.style.backgroundColor = "black";
+                    if (randomState === true) {
+                        pixel.style.backgroundColor = `${randomColor()}`;
+                    } else {pixel.style.backgroundColor = 'black'};
                     e.preventDefault();
                 }
             })
             pixel.addEventListener("mouseover", () => {
                 if (mouseState === true) {
-                    pixel.style.backgroundColor = "black";
+                    if (randomState === true) {
+                        pixel.style.backgroundColor = `${randomColor()}`;
+                    } else {pixel.style.backgroundColor = 'black'};
                 }
-            })
-            pixel.addEventListener("mouseup", () => {
-                mouseState = false;
             })
         }
     }) 
+    
     function calculatePixelSide () {
         return Math.round((250/input.value) * 1000) / 1000
     }
 }
 
-input.addEventListener('input', () => {
+input.addEventListener("input", () => {
     const pixels = document.querySelectorAll('.container > *');
     pixels.forEach(p => {
         p.remove();
     });
     createGrid(input.value);
 })
+
+rainbow.addEventListener("click", () => {
+    randomState = true});
+
+function randomColor() {   //returns a random rgb color code
+  function randomNumber() {
+    return Math.floor(Math.random() * 256)
+  }
+
+  return `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`
+}    
+    
+pencil.addEventListener("click", () => {
+    randomState = false}); 
 
 createGrid(input.value);
 
